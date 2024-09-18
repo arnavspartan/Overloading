5f77a66fd1dacd521d5987639e3b5e239569166a
Arnav Thakur <bus.arnavt@gmail.com>
2024-09-18 11:26:49 +0530

    Add files via upload

---
+++ overL.java
@@ -0,0 +1,19 @@
+package Practice.Overloading;
+
+public class overL {
+
+    void add(int a) {
+
+        int c;
+        int b = 10;
+        c=a+b;
+        System.out.println(c);
+    }
+
+    void add(int a, int b) {
+
+        int c;
+        c=a+b;
+        System.out.println(c);
+    }
+}
---
+++ overLoading.java
@@ -0,0 +1,11 @@
+package Practice.Overloading;
+
+public class overLoading {
+    public static void main(String[] args) {
+        overL over = new overL();
+        over.add(12);
+        over.add(12,26);
+    }    
+}
+
+
